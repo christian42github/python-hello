0227b4521d814210b2449f5706219c5eb8b9c6ae
Christian Bauermeister <christianb@Christians-Mac-mini.local>
2026-04-03 15:06:30 +0200

    update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-hello
+
+this is the repo for the AWS tutorial on "APP runner"
---
+++ setup_repo.sh
@@ -0,0 +1,8 @@
+echo "# python-hello" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/christian42github/python-hello.git
+git push -u origin main
+
